591d15b5b22c2ff35dee1c85602f64ebc1fb4ae8
8bitjustin <j.olson.digital@gmail.com>
2019-08-20 17:58:43 -0700

    Added code to write/save image file.

--- script01.py
+++ script01.py
@@ -20,5 +20,7 @@ resized_image = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
 
 # displays image in window
 cv2.imshow("Galaxy", resized_image)
+# writes image and saves
+cv2.imwrite("Galaxy_resized.jpg", resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
